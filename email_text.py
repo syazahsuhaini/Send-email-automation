87d26ae0a8c431520b38b79df3b31675f337316e
syazahsuhaini <syazasuhaini@gmail.com>
2026-01-10 12:01:15 +0800

    version 1.0 - final

--- email_text.py
+++ email_text.py
@@ -1,6 +1,6 @@
 
 def text_template(month, year):
-        text_temp = (f'Dear AAA/BBB/CCC,\n\n'
+        text_temp = (f'Dear aaa/bbb/ccc,\n\n'
                         f'Below I attached all the timesheets for {month.capitalize()} {year}.\n\n'
                         f'Thank you.\n\nBest regards,\nSyaza\n000-0000000')
         
